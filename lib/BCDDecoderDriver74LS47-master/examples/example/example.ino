8613ce024bc486c9281c2085c7f4f383bb3e98ea
udonhef2bmad <udonhef2bmad@gmail.com>
2022-09-01 19:22:59 +0200

    added timer and 7 segment display

--- lib/BCDDecoderDriver74LS47-master/examples/example/example.ino
+++ lib/BCDDecoderDriver74LS47-master/examples/example/example.ino
@@ -20,7 +20,7 @@ void setup()
 
 void loop()
 {
-  for (int i = 0; i >= 9; i++)
+  for (int i = 0; i <= 9; i++)
   {
     sevenSegDisplay.setNumber(i);
   }
--- src/main.cpp
+++ src/main.cpp
@@ -1,108 +1,183 @@
 #include <Arduino.h>
 #include <Arduino_FreeRTOS.h>
 
+#include <BCDDecoderDriver74LS47.h>
+
+#define pinA 4
+#define pinB 5
+#define pinC 6
+#define pinD 7
+
+#define pinRBI 9
+
+BCDDecoderDriver74LS47 sevenSegDisplay(pinA, pinB, pinC, pinD, pinRBI);
+
+unsigned long countdownStart = 0;         // countdown start in ms, used to offset countdown
+unsigned long countdownDuration = 300000; // countdown duration in ms (5 minutes)
+
+#define seven_seg_count 4
+char seven_seg_en[seven_seg_count] = {2, 3, 10, 11}; //seven segment pin definition
+char seven_seg_char[seven_seg_count] = {0,0,0,0}; //shared from timer to 7 seg display
+
+// returns coutdown since start in ms
+// countdown stops at 0
+unsigned long getCountDown()
+{
+    unsigned long currentTime = xTaskGetTickCount();
+    unsigned long deadline = (countdownDuration + countdownStart) / portTICK_PERIOD_MS;
+
+    if (currentTime > deadline)
+    {
+        // countdown reached, return 0;
+        return 0;
+    }
+    // countdown ongoing, return time left
+    return (deadline - currentTime) * portTICK_PERIOD_MS;
+}
+
 // define two tasks for Blink & AnalogRead
-void TaskBlink( void *pvParameters );
-void TaskAnalogRead( void *pvParameters );
+void TaskSevenSeg(void *pvParameters);
+void TaskTimer(void *pvParameters);
+void TaskAnalogRead(void *pvParameters);
 
 // the setup function runs once when you press reset or power the board
-void setup() {
-  
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
-  
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
-  }
+void setup()
+{
 
-  // Now set up two tasks to run independently.
-  xTaskCreate(
-    TaskBlink
-    ,  "Blink"   // A name just for humans
-    ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
-    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL );
+    sevenSegDisplay.display(true);
 
-  xTaskCreate(
-    TaskAnalogRead
-    ,  "AnalogRead"
-    ,  128  // Stack size
-    ,  NULL
-    ,  1  // Priority
-    ,  NULL );
+    // iterate over every display
+    for (int i = 0; i < seven_seg_count; i++)
+    {
+        pinMode(seven_seg_en[i], OUTPUT);
+        digitalWrite(seven_seg_en[i], HIGH);
+    }
 
-  // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
+    // initialize serial communication at 9600 bits per second:
+    Serial.begin(115200);
+
+    while (!Serial)
+    {
+        ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
+    }
+    Serial.print("Serial intialized. creating tasks...");
+
+    // Now set up two tasks to run independently.
+    xTaskCreate(
+        TaskSevenSeg, "TaskSevenSeg" // A name just for humans
+        ,
+        1024 // This stack size can be checked & adjusted by reading the Stack Highwater
+        ,
+        NULL, 3 // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+        ,
+        NULL);
+
+    xTaskCreate(
+        TaskTimer, "TaskTimer" // A name just for humans
+        ,
+        1024 // This stack size can be checked & adjusted by reading the Stack Highwater
+        ,
+        NULL, 2 // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+        ,
+        NULL);
+
+
+
+    // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
+    // Serial.print("starting scheduler");
+    // vTaskStartScheduler();
+    // Serial.print("insufficient ram!");
 }
 
 void loop()
 {
-  // Empty. Things are done in Tasks.
+    // Empty. Things are done in Tasks.
 }
 
 /*--------------------------------------------------*/
 /*---------------------- Tasks ---------------------*/
 /*--------------------------------------------------*/
 
-void TaskBlink(void *pvParameters)  // This is a task.
+void TaskAnalogRead(void *pvParameters) // This is a task.
 {
-  (void) pvParameters;
+    (void)pvParameters;
 
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
 
-  Most Arduinos have an on-board LED you can control. On the UNO, LEONARDO, MEGA, and ZERO 
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN takes care 
-  of use the correct LED pin whatever is the board used.
-  
-  The MICRO does not have a LED_BUILTIN available. For the MICRO board please substitute
-  the LED_BUILTIN definition with either LED_BUILTIN_RX or LED_BUILTIN_TX.
-  e.g. pinMode(LED_BUILTIN_RX, OUTPUT); etc.
-  
-  If you want to know what pin the on-board LED is connected to on your Arduino model, check
-  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
-  
-  This example code is in the public domain.
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        Serial.print("kek");
+    for(;;)
+    {
+    }
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-*/
-
-  // initialize digital LED_BUILTIN on pin 13 as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  for (;;) // A Task shall never return or exit.
-  {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
-  }
 }
 
-void TaskAnalogRead(void *pvParameters)  // This is a task.
+void TaskMaster(void *pvParameters)
 {
-  (void) pvParameters;
-  
-/*
-  AnalogReadSerial
-  Reads an analog input on pin 0, prints the result to the serial monitor.
-  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
-
-  This example code is in the public domain.
-*/
-
-  for (;;)
-  {
-    // read the input on analog pin 0:
-    int sensorValue = analogRead(A0);
-    // print out the value you read:
-    Serial.println(sensorValue);
-    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
-  }
+    (void)pvParameters;
 }
+
+char getdigit(unsigned long number, unsigned short nth_digit)
+{
+    while (nth_digit--)
+    {
+        number /= 10;
+    }
+    return (number % 10) + '0';
+}
+
+
+void TaskTimer(void *pvParameters) // continually updates the 7segment display
+{
+    (void)pvParameters;
+
+    while (1)
+    {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        unsigned long time_left_cs = getCountDown() / 10; // centiseconds
+        unsigned long time_left_s = time_left_cs / 100;   // seconds
+        unsigned long time_left_m = time_left_s / 60;     // minutes
+
+        unsigned long time_left_ss = time_left_s % 60;   // seconds mod 60
+        unsigned long time_left_mm = time_left_m % 60;     // minutes mod 60
+
+        // if over a minute is left, display mm:ss
+        if (time_left_s > 60)
+        {
+            seven_seg_char[0] = getdigit(time_left_ss, 0); // seconds
+            seven_seg_char[1] = getdigit(time_left_ss, 1); // tens of seconds
+            seven_seg_char[2] = getdigit(time_left_mm, 0); // minutes
+            seven_seg_char[3] = getdigit(time_left_mm, 1); // tens of minutes
+        }
+        else // if under a minute is left, diplay ss:cs
+        {
+            seven_seg_char[0] = getdigit(time_left_cs, 0); // centiseconds
+            seven_seg_char[1] = getdigit(time_left_cs, 1); // tens of centiseconds
+            seven_seg_char[2] = getdigit(time_left_ss, 0);  // seconds
+            seven_seg_char[3] = getdigit(time_left_ss, 1);  // tens of seconds
+        }
+
+        Serial.print("Task1 : ");
+        Serial.print(seven_seg_char[3]);
+        Serial.print(seven_seg_char[2]);
+        Serial.print(":");
+        Serial.print(seven_seg_char[1]);
+        Serial.println(seven_seg_char[0]);
+    }
+}
+
+void TaskSevenSeg(void *pvParameters) // continually updates the 7segment display
+{
+    (void)pvParameters;
+
+    while (1)
+    {
+        for(int i = 0; i < seven_seg_count; i++)
+        {
+            digitalWrite(seven_seg_en[i], LOW);
+            sevenSegDisplay.setNumber(seven_seg_char[0]);
+            digitalWrite(seven_seg_en[i], HIGH);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+        }
+    }
+}
+
